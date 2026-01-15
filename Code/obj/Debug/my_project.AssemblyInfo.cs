ee2995a8581cc0ab37d39c5f62d1d3d90ef56d9f
aadrezmoose <austinddrez@gmail.com>
2026-01-15 17:39:50 -0600

    Purpose defeated

--- Code/obj/Debug/my_project.AssemblyInfo.cs
+++ Code/obj/Debug/my_project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("my_project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71f905e0c8208e622903fa8bebfa3fc413e5ca78")]
 [assembly: System.Reflection.AssemblyProductAttribute("my_project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("my_project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
